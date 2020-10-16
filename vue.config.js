c4942723285a6cc789dee78b6af6dca5cc99b7d1
Eskimon <eskimon@outlook.com>
2020-10-16 17:30:59 +0200

    First public version

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '.' : '/',
+  // UglyTrick: Output to "docs/" instead of "dist/" to easily serve the project from github pages
+  outputDir: 'docs',
 }
-
